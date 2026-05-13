16dabe00158eb78206b9a6cb33cfd1f3bdcd9dfa
Nishant Bhickta <nishantbhickta@gmail.com>
2026-05-13 16:36:58 +0000

    refactor sales order invoice helpers out of posapp

--- posawesome/posawesome/api/posapp.py
+++ posawesome/posawesome/api/posapp.py
@@ -26,7 +26,6 @@ from erpnext.accounts.doctype.payment_request.payment_request import (
     get_existing_payment_request_amount,
 )
 
-from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
 from erpnext.accounts.doctype.loyalty_program.loyalty_program import (
     get_loyalty_program_details_with_points,
 )
@@ -38,6 +37,10 @@ from posawesome.posawesome.api.sales_order_lookup import search_sales_orders
 from posawesome.posawesome.api.quotation_lookup import (
     require_quotation_permission as _require_quotation_permission,
     search_pos_quotations,
+)
+from posawesome.posawesome.api.sales_order_invoice import (
+    make_or_get_sales_invoice_from_order,
+    update_invoice_from_order_data,
 )
 from frappe.utils.caching import redis_cache
 
@@ -1902,17 +1905,7 @@ def add_taxes_from_tax_template(item, parent_doc):
 
 @frappe.whitelist()
 def update_invoice_from_order(data):
-    data = json.loads(data)
-    if data.get("name"):
-        invoice_doc = frappe.get_doc("Sales Invoice", data.get("name"))
-        invoice_doc.update(data)
-    else:
-        data.pop("name", None)
-        invoice_doc = frappe.get_doc(data)
-    invoice_doc.flags.ignore_permissions = True
-    frappe.flags.ignore_account_permission = True
-    invoice_doc.save()
-    return invoice_doc
+    return update_invoice_from_order_data(data)
 
 
 @frappe.whitelist()
@@ -3958,51 +3951,9 @@ def get_seearch_items_conditions(item_code, serial_no, batch_no, barcode):
 
 @frappe.whitelist()
 def create_sales_invoice_from_order(sales_order, pos_profile=None, pos_opening_shift=None):
-    existing_invoice = _get_draft_invoice_for_sales_order(
+    return make_or_get_sales_invoice_from_order(
         sales_order, pos_profile=pos_profile, pos_opening_shift=pos_opening_shift
     )
-    if existing_invoice:
-        return frappe.get_doc("Sales Invoice", existing_invoice).as_dict()
-
-    sales_invoice = make_sales_invoice(sales_order, ignore_permissions=True)
-    return sales_invoice.as_dict()
-
-
-def _get_draft_invoice_for_sales_order(sales_order, pos_profile=None, pos_opening_shift=None):
-    sales_order = cstr(sales_order or "").strip()
-    if not sales_order:
-        return ""
-
-    conditions = [
-        "si.docstatus = 0",
-        "ifnull(si.is_pos, 0) = 1",
-        "sii.sales_order = %s",
-    ]
-    values = [sales_order]
-
-    pos_profile = cstr(pos_profile or "").strip()
-    if pos_profile:
-        conditions.append("si.pos_profile = %s")
-        values.append(pos_profile)
-
-    pos_opening_shift = cstr(pos_opening_shift or "").strip()
-    if pos_opening_shift and frappe.db.has_column("Sales Invoice", "posa_pos_opening_shift"):
-        conditions.append("si.posa_pos_opening_shift = %s")
-        values.append(pos_opening_shift)
-
-    rows = frappe.db.sql(
-        """
-        select distinct si.name
-        from `tabSales Invoice` si
-        inner join `tabSales Invoice Item` sii on sii.parent = si.name
-        where {conditions}
-        order by si.modified desc
-        limit 1
-        """.format(conditions=" and ".join(conditions)),
-        values,
-        as_dict=True,
-    )
-    return rows[0].name if rows else ""
 
 
 @frappe.whitelist()
